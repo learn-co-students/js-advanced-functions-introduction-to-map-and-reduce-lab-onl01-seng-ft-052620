c1a3b3acf5da0d30de8f58e4cfa8be36d0c7ceab
charlie763 <cwisoff@gmail.com>
2020-09-01 11:22:54 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-// Your code here
+function mapToNegativize(ary){
+  const newAry = [];
+  for (const num of ary){
+    newAry.push(num*-1);
+  }
+  return newAry;
+}
+
+function mapToNoChange(ary){return ary}
+
+function myOwnMap(ary, mapFn){
+  const newAry = [];
+  for (const num of ary){
+    newAry.push(mapFn(num));
+  }
+  return newAry;
+}
+
+function mapToDouble(ary){
+  return myOwnMap(ary, (num)=> num*2)
+}
+
+function mapToSquare(ary){
+  return myOwnMap(ary, (num)=> num**2)
+}
+
+function reduceToTotal(ary, memo=0){
+  for (const num of ary){
+    memo += num;
+  }
+  return memo;
+}
+
+function myOwnReduce(ary, reduceFn, memo){
+  let newMemo = memo;
+  for (const element of ary){
+    newMemo = reduceFn(element, newMemo)
+  }
+  return newMemo
+}
+
+function reduceToAllTrue(ary){
+  let memo = true;
+  return myOwnReduce(ary, 
+    function(element, memo){if(memo==true){return !!element}},
+    true 
+  )
+}
+
+function reduceToAnyTrue(ary){
+  let memo = false;
+  const anyFn = function(element, memo){
+    if(!memo){
+      return !!element;
+    }
+  }
+  return myOwnReduce(ary, anyFn, false)
+}
+
