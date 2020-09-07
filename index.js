89e05b276c92ca008fc084989e95b874ba502cdf
barrettanderson <barrettanderson1989@gmail.com>
2020-09-07 10:04:19 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+
+function mapToNegativize(array) {
+    return array.map(i => i * -1)
+}
+
+function mapToNoChange(array) {
+    return array.map(i => i)
+}
+
+function mapToDouble(array) {
+    return array.map(i => i * 2)
+}
